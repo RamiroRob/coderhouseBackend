9682cf73eb2d4de4cc329ffa25e04d97906a2ec5
Ramiro Roballos <ramiroroballos@gmail.com>
2023-05-22 22:58:44 -0400

    terminado

--- Backend/M1/Entregable2_ProductManager.js
+++ Backend/M1/Entregable2_ProductManager.js
@@ -38,10 +38,11 @@ class ProductManager {
     }
 
     getProducts() {
+        if (!fs.existsSync(this.path)) {
+            return [] 
+        } 
+
         const productos = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
-        if(productos.length == 0) {
-            throw new Error('No products found');
-        }
 
         return productos
     }
@@ -72,19 +73,15 @@ class ProductManager {
     }
 }
 
+
+// Testing
 const productManager = new ProductManager();
 
-productManager.addProduct("producto prueba 1", "Este es un producto prueba", 200, "Sin imagen", "a", 25)
-productManager.addProduct("producto prueba 2", "Este es un producto prueba 3", 300, "Sin imagen", "b", 50)
-productManager.addProduct("producto prueba 3", "Este es un producto prueba 4", 400, "Sin imagen", "c", 50)
-productManager.addProduct("producto prueba 4", "Este es un producto prueba 5", 500, "Sin imagen", "d", 50)
-
-console.log("Primer log --------", productManager.getProducts())
-
+console.log("Primer getProducts", productManager.getProducts())
+productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
+console.log("getProducts con un producto creado", productManager.getProducts())
+console.log("getProductById", productManager.getProductById(1))
 productManager.updateProduct(1, "title", "newTitle12345")
-console.log("Segundo log con nuevo titulo en 1 --------",productManager.getProducts())
-
-console.log("getProductbyId 3 --------", productManager.getProductById(3))
-
-productManager.deleteProduct(2)
-console.log("Delete id 2 --------", productManager.getProducts())
+console.log("updatedProduct", productManager.getProducts()) 
+productManager.deleteProduct(1)
+console.log("Deleted Product", productManager.getProducts())
