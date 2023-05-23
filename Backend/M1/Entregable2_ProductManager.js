ae42ee1015a57dbcaa51e1707cacaa4f46cb07b2
Ramiro Roballos <ramiroroballos@gmail.com>
2023-05-22 19:06:56 -0500

    entregable 2 terminado

--- Backend/M1/Entregable2_ProductManager.js
+++ Backend/M1/Entregable2_ProductManager.js
@@ -7,14 +7,21 @@ class ProductManager {
         this.path = 'productos.txt'
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
-  
-        if (this.products.find(product => product.code == code)) {
-            throw new Error('Code already exists');
-        }
 
-        if (!title || !description || !price || !thumbnail || !code || !stock) {
-            throw new Error('Missing data');
+    
+    addProduct(title, description, price, thumbnail, code, stock) {
+
+        if(fs.existsSync(this.path)){
+
+            const productos = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
+
+            if (productos.find(product => product.code == code)) {
+                throw new Error('Code already exists');
+            }
+            
+            if (!title || !description || !price || !thumbnail || !code || !stock) {
+                throw new Error('Missing data');
+            }
         }
 
         this.products.push({
@@ -31,18 +38,17 @@ class ProductManager {
     }
 
     getProducts() {
-        if(this.products.length == 0) {
+        const productos = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
+        if(productos.length == 0) {
             throw new Error('No products found');
         }
 
-        const data = fs.readFileSync(this.path, 'utf-8');
-        return JSON.parse(data)
+        return productos
     }
 
     getProductById(id) {
 
         const productos = JSON.parse(fs.readFileSync(this.path, 'utf-8'))
-        console.log(productos)
         let product = productos.find(product => product.id == id);
 
         if(!product) {
@@ -53,29 +59,32 @@ class ProductManager {
     }  
 
     updateProduct(id, field, content) {
+
         const productos = JSON.parse(fs.readFileSync(this.path, "utf-8"))
         productos[id-1][field] = content
         fs.writeFileSync(this.path, JSON.stringify(productos))
     }
 
     deleteProduct(id) {
-        this.products[id-1] = null
+
+        let productos = JSON.parse(fs.readFileSync(this.path, 'utf-8')).filter(product => product.id !== id)
+        fs.writeFileSync(this.path, JSON.stringify(productos))
     }
 }
 
 const productManager = new ProductManager();
 
-productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25)
-productManager.addProduct("producto prueba 2", "Este es un producto prueba 2", 300, "Sin imagen", "abc1234", 50)
+productManager.addProduct("producto prueba 1", "Este es un producto prueba", 200, "Sin imagen", "a", 25)
+productManager.addProduct("producto prueba 2", "Este es un producto prueba 3", 300, "Sin imagen", "b", 50)
+productManager.addProduct("producto prueba 3", "Este es un producto prueba 4", 400, "Sin imagen", "c", 50)
+productManager.addProduct("producto prueba 4", "Este es un producto prueba 5", 500, "Sin imagen", "d", 50)
 
-console.log("Primer log", productManager.getProducts())
+console.log("Primer log --------", productManager.getProducts())
+
 productManager.updateProduct(1, "title", "newTitle12345")
-console.log("Segundo log",productManager.getProducts())
+console.log("Segundo log con nuevo titulo en 1 --------",productManager.getProducts())
 
-console.log("getProductbyId--------", productManager.getProductById(1))
-// productManager.getProductById(2)
+console.log("getProductbyId 3 --------", productManager.getProductById(3))
 
-// productManager.deleteProduct(1)
-// productManager.deleteProduct(2)
-// productManager.deleteProduct(3)
-// console.log(productManager.getProducts())
+productManager.deleteProduct(2)
+console.log("Delete id 2 --------", productManager.getProducts())
