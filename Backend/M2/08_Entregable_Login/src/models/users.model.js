872f6b53d0dadc3ff2637b0a30157e7e08692cf7
Ramiro Roballos <ramiroroballos@gmail.com>
2023-07-23 15:05:49 -0400

    fix

--- Backend/M2/08_Entregable_Login/src/models/users.model.js
+++ Backend/M2/08_Entregable_Login/src/models/users.model.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
     last_name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ['admin', 'user'], default: 'user' },
+    role: { type: String, enum: ['admin', 'usuario'], default: 'usuario' },
 });
 
 const userModel = mongoose.model(userCollection, userSchema);
