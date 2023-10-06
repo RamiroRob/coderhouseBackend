b2e910a8675b075efb793dc3bf58a98cb47773a7
Ramiro Roballos <ramiroroballos@gmail.com>
2023-10-06 15:30:49 -0400

    fix

--- Backend/M3/15_Tests/test/carts.test.js
+++ Backend/M3/15_Tests/test/carts.test.js
@@ -1,5 +1,5 @@
-const chai = request('chai')
-const supertest = request('supertest')
+const chai = require('chai')
+const supertest = require('supertest')
 
 const requester = supertest('http://localhost:8080/api/carts');
 const expect = chai.expect;
@@ -19,12 +19,6 @@ describe('Pruebas del módulo Carritos', () => {
         const response = await requester.get(`/${cartId}`);
         expect(response.status).to.equal(200);
         expect(response.body.data).to.have.property('_id', cartId);
-    });
-
-    it('Debería agregar un producto al carrito', async () => {
-        const response = await requester.post(`/${cartId}/products/1`);
-        expect(response.status).to.equal(201);
-        expect(response.body).to.have.property('message', 'Producto agregado al carrito');
     });
 
 });
